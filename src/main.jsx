6ac2ee184acce145f9dcb62fc93a866bd14a3311
Hatimhjamali <110445837+Hatimhjamali@users.noreply.github.com>
2026-03-05 09:24:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./studio.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
